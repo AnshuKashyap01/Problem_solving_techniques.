7f2fc78f17928df9625477873aff2781d9cc8975
Anshu Kashyap <contactanshu247@gmail.com>
2026-08-16 23:30:37 +0530

    Time: 1 ms (82.74%) | Memory: 131.3 MB (33.50%) - LeetSync

---
+++ 2156-stone-game-ix/stone-game-ix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        int c0 = cnt[0];
+        int c1 = cnt[1];
+        int c2 = cnt[2];
+
+        
+        if (min(c1, c2) == 0) {
+            return max(c1, c2) > 2 && c0 % 2 == 1;
+        }
+
+       
+        return abs(c1 - c2) > 2 || c0 % 2 == 0;
+    }
+};
